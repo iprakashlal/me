17df3714215d151ca0782c98d21102abd97ba42c
Prakash <40267496+iprakashlal@users.noreply.github.com>
2020-04-19 13:02:56 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,79 @@
+var projectInputs = [{
+    "label": 'Cura',
+    keyTechs: '<ul><li>PHP, file system</li><li>MySQL</li><li>HTML, CSS</li></ul>',
+    techSkills: '<ul><li>Data table</li><li>Server management</li></ul>',
+    description: 'Our objective was to handle the vigorous management of profiles being registered to an entity',
+    projectLink: "#soon",
+    ctaButton: 'Visit the project',
+    idContainer: 'projectContentZone'
+}, {
+    label: 'Attendance recorder',
+    keyTechs: '<ul><li>PHP</li><li>MySQL</li><li>Bootstrap framework</li><li>HTML, CSS</li></ul>',
+    techSkills: '<ul><li>Data table</li></li>MVC framework<li></ul>',
+    description: 'Our objective was to handle the vigorous management of profiles being registered to an entity',
+    projectLink: "#soon",
+    ctaButton: 'Visit the project',
+}]
+
+
+var skillInputs = [{
+    score: '95',
+    label: 'HTML',
+    idContainer: 'skillContentZone'
+},
+{
+    score: '90',
+    label: 'CSS'
+},
+{
+    score: '75',
+    label: 'Vanilla Javascript'
+},
+{
+    score: '70',
+    label: 'OOPS concept'
+}, {
+    score: '75',
+    label: 'Analytics'
+}
+]
+
+var subSkillInputs = [{
+    score: '75',
+    label: 'Adobe creative cloud',
+    idContainer: 'subSkillContentZone'
+},
+{
+    score: '70',
+    label: 'Strategy and logic'
+},
+{
+    score: '75',
+    label: 'Cvent'
+},
+{
+    score: '50',
+    label: 'Google Analytics'
+}, {
+    score: '70',
+    label: 'AEM UI management'
+}, {
+    score: '80',
+    label: 'Web analysis'
+}, {
+    score: '75',
+    label: 'GIT'
+}, {
+    score: '60',
+    label: 'Meteor JS'
+}, {
+    score: '75',
+    label: 'Project Management'
+}, {
+    score: '75',
+    label: 'Agile Development'
+}, {
+    score: '70',
+    label: 'CMS Development'
+}
+]
