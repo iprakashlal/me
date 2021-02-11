45dcc46f891f0b748c0805190d280bcbf57eb2f8
Prakash <40267496+iprakashlal@users.noreply.github.com>
2021-02-12 00:40:29 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,18 @@
 var projectInputs = [{
+    "label": 'Biometric system',
+    keyTechs: '<ul><li>PHP, file system</li><li>MySQL</li><li>Web application</li><li>Process analysis workflow</li></ul>',
+    techSkills: '<ul><li>Web framework</li><li>ASP.net</li><li>HTML/CSS/Jquery</li></ul>',
+    description: 'Our objective was to handle the vigorous management of profiles being registered to an entity',
+    projectLink: "#soon",
+    ctaButton: 'Comming soon on approval',
+    idContainer: 'projectContentZone'
+},{
     "label": 'Cura',
     keyTechs: '<ul><li>PHP, file system</li><li>MySQL</li><li>HTML, CSS</li></ul>',
     techSkills: '<ul><li>Data table</li><li>Server management</li></ul>',
     description: 'Our objective was to handle the vigorous management of profiles being registered to an entity',
     projectLink: "#soon",
-    ctaButton: 'Visit the project',
+    ctaButton: 'Comming soon on approval',
     idContainer: 'projectContentZone'
 }, {
     label: 'Attendance recorder',
@@ -12,7 +20,21 @@ var projectInputs = [{
     techSkills: '<ul><li>Data table</li></li>MVC framework<li></ul>',
     description: 'Our objective was to handle the vigorous management of profiles being registered to an entity',
     projectLink: "#soon",
-    ctaButton: 'Visit the project',
+    ctaButton: 'Comming soon on approval',
+}, {
+    label: 'V. K. Lalco Pvt. Ltd.',
+    keyTechs: '<ul><li>PHP</li><li>MySQL</li><li>Bootstrap framework</li><li>HTML, CSS</li><li>Magento</li><li>Drupal</li></ul>',
+    techSkills: '<ul><li>E-commerce</li><li>payment gateways</li><li>E-commerce process flow</li><li>Google analytic management</li></ul>',
+    description: 'My objective was to handle the E-commerce site & its backend/frontend engine',
+    projectLink: "#soon",
+    ctaButton: 'Comming soon on approval',
+}, {
+    label: 'Mindcraft Software Pvt. Ltd.',
+    keyTechs: '<ul><li>IntelliJ</li><li>ASP.net</li><li>Foundation & Boostrap framework</li><li>Meteor & Node & Angular JS</li><li>Mongo DB</li><li>Core CSS</li></ul>',
+    techSkills: '<ul><li>Core frontend specialist</li><li>Frontend with JAva integration</li><li>SVN services</li></ul>',
+    description: 'My objective was to handle the core frontend developement on banking & medical sector applications',
+    projectLink: "#soon",
+    ctaButton: 'Comming soon on approval',
 }]
 
 
